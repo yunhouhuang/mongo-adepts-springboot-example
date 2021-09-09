8d16535b7e38539d2cd81cf4356867934586485d
nalhoug <Me914374385>
2021-09-09 21:22:23 +0800

    1.0.7 lambada

---
+++ src/main/java/com/example/mongo/adepts/springboot/example/controller/LambadaTestController.java
@@ -0,0 +1,32 @@
+package com.example.mongo.adepts.springboot.example.controller;
+
+import com.coeuy.osp.mongo.adepts.model.query.Adepts;
+import com.coeuy.osp.mongo.adepts.model.query.LambdaQueryAdepts;
+import com.coeuy.osp.mongo.adepts.model.query.QueryAdepts;
+import com.coeuy.osp.mongo.adepts.service.MongoAdepts;
+import com.example.mongo.adepts.springboot.example.model.document.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * Explain
+ * </p>
+ *
+ * @author yarnk
+ * @date 2021/9/9
+ */
+@RestController
+@RequiredArgsConstructor
+public class LambadaTestController {
+
+    private final MongoAdepts mongoAdepts;
+
+    @GetMapping("lambada/{username}")
+    public String lamb(@PathVariable String username){
+        User superman = mongoAdepts.getOne(Adepts.<User>lambdaQuery().eq(User::getUsername, username), User.class);
+        return superman.toString();
+    }
+}
